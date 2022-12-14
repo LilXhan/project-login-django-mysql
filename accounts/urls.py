ea43670617eb49b8a146f2e10f7b1ec343d7a9f4
Xhan <alvaradotuctod@gmail.com>
2022-12-14 07:58:07 -0500

    add-register

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from django.contrib.auth.views import LoginView
+from . import views
+
+urlpatterns = [
+    path('', LoginView.as_view(), name='login'),
+    path('/register', views.RegisterUser.as_view(), name='register')
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from .forms import SingUpUserForm
 
-# Create your views here.
+class RegisterUser(CreateView):
+    template_name = 'registration/register.html'
+    form_class = SingUpUserForm
+
+    def form_valid(self, form):
+        form.save()
+
+        return redirect('login')
